1a3a7c0ad7befa3bb0197703cbe96a70bd56944e
reismahnic <reismahnic@gmail.com>
2017-08-03 15:15:37 -0700

    Initial Commit

---
+++ StringExercises/StringExercises/StringExercises.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StringExercises.aspx.cs" Inherits="StringExercises.StringExercises" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+
+            Exercise 1 result:
+            <asp:Label ID="Exercise1lbl" runat="server"></asp:Label>
+            <br />
+            Exercise 2 result:
+            <asp:Label ID="Exercise2lbl" runat="server"></asp:Label>
+            <br />
+            Exercise 3 result:
+            <br />
+            <asp:Label ID="Exercise3lbl" runat="server"></asp:Label>
+            <br />
+            Exercise 4 result: <asp:Label ID="Exercise4lbl" runat="server"></asp:Label>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ StringExercises/StringExercises/StringExercises.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StringExercises
+{
+    public partial class StringExercises : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //Challenge 1: Reverse my name
+            string name = "Reis Mahnic";
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                Exercise1lbl.Text += name[i];
+            }
+
+            //Challenge 2: Reverse a list of names
+            string names = "Luke,Leia,Han,Chewbacca";
+
+            string[] nameList = names.Split(',');
+            string results = "";
+            for (int i = nameList.Length - 1; i >= 0; i--)
+            {
+                results += nameList[i] + ",";
+            }
+            results = results.Remove(results.Length - 1, 1);
+            Exercise2lbl.Text = results;
+
+            //Challenge 3: List of names with ASCII art
+            string art = "";
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                int leftSide = (14 - nameList[i].Length) / 2;
+                string padding = nameList[i].PadLeft(nameList[i].Length + leftSide, '*');
+                art += padding.PadRight(14, '*');
+                art += "<br />";
+                Exercise3lbl.Text = art;
+            }
+
+            //Challenge 4: Puzzle
+
+            string puzzle = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF THEIR COUNTRY";
+            puzzle = puzzle.ToLower();
+            puzzle = puzzle.Replace("z", "t");
+            puzzle = puzzle.Remove(0, 1);
+            puzzle = puzzle.Insert(0, "N");
+            puzzle = puzzle.Remove(10, 9);
+           
+
+
+            Exercise4lbl.Text = puzzle;
+        }
+    }
+}
